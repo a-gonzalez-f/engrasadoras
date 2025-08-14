4d700bc2680a28d299b1931acb971ba26051f066
a-gonzalez-f <a_gonzalezf@outlook.com>
2025-08-14 09:45:50 -0300

    cambio campos requeridos

--- src/models/gateway.js
+++ src/models/gateway.js
@@ -7,7 +7,7 @@ const GatewaySchema = new mongoose.Schema(
     ip: { type: String, required: true },
     nombre: { type: String, default: "-" },
     id: { type: Number, required: true },
-    linea: { type: String, required: true },
+    linea: { type: String, default: "-" },
     ubicacion: { type: String, default: "-" },
 
     power: { type: Boolean, default: false },
