edaa0ec9c64ae50761c7d1df242f2ad693b2df69
a-gonzalez-f <a_gonzalezf@outlook.com>
2025-09-17 10:04:33 -0300

    paso a valor absoluto la señal

--- public/engrasadoras/formatear-signal.js
+++ public/engrasadoras/formatear-signal.js
@@ -1,13 +1,14 @@
 export function formatearSignal(signal, modo = "icono") {
   let estado = "";
+  let signal_abs = -Math.abs(signal);
 
-  if (signal <= -1 && signal > -80) {
+  if (signal_abs <= -1 && signal_abs > -80) {
     estado = "excelente";
-  } else if (signal <= -80 && signal > -90) {
+  } else if (signal_abs <= -80 && signal_abs > -90) {
     estado = "buena";
-  } else if (signal <= -90 && signal > -100) {
+  } else if (signal_abs <= -90 && signal_abs > -100) {
     estado = "pobre";
-  } else if (signal <= -100) {
+  } else if (signal_abs <= -100) {
     estado = "sinsenal";
   } else {
     estado = "sinsenal";
@@ -16,19 +17,19 @@ export function formatearSignal(signal, modo = "icono") {
   const estadosMap = {
     excelente: {
       texto: "Excelente",
-      icono: `<span title="${signal}" class="material-symbols-outlined" style="color:var(--color-pstv)">signal_cellular_alt</span>`,
+      icono: `<span title="${signal_abs}" class="material-symbols-outlined" style="color:var(--color-pstv)">signal_cellular_alt</span>`,
     },
     buena: {
       texto: "Buena",
-      icono: `<span title="${signal}" class="material-symbols-outlined" style="color:var(--color-pstv)">signal_cellular_alt_2_bar</span>`,
+      icono: `<span title="${signal_abs}" class="material-symbols-outlined" style="color:var(--color-pstv)">signal_cellular_alt_2_bar</span>`,
     },
     pobre: {
       texto: "Pobre",
-      icono: `<span title="${signal}" class="material-symbols-outlined" style="color:var(--color-alerta)">signal_cellular_alt_1_bar</span>`,
+      icono: `<span title="${signal_abs}" class="material-symbols-outlined" style="color:var(--color-alerta)">signal_cellular_alt_1_bar</span>`,
     },
     sinsenal: {
       texto: "Sin señal",
-      icono: `<span title="${signal}" class="material-symbols-outlined" style="color:var(--color-error)">signal_cellular_connected_no_internet_0_bar</span>`,
+      icono: `<span title="${signal_abs}" class="material-symbols-outlined" style="color:var(--color-error)">signal_cellular_connected_no_internet_0_bar</span>`,
     },
   };
 
